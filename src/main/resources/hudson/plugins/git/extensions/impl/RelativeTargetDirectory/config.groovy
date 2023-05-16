f7f0fb23dc21ab36671548ca9ced4d720709012d
bright.ma <bright.ma@magesfc.com>
2023-05-16 16:28:24 +0800

    src: resources: config.groovy: add field relativeToTmp
    src/main/resources/hudson/plugins/git/extensions/impl/RelativeTargetDirectory

--- src/main/resources/hudson/plugins/git/extensions/impl/RelativeTargetDirectory/config.groovy
+++ src/main/resources/hudson/plugins/git/extensions/impl/RelativeTargetDirectory/config.groovy
@@ -5,3 +5,6 @@ def f = namespace(lib.FormTagLib)
 f.entry(title:_("Local subdirectory for repo"), field:"relativeTargetDir") {
     f.textbox()
 }
+f.entry(field: "relativeToTmp") {
+    f.checkbox(title: _("relative to  workspace@tmp dir"))
+}
